5cd021eaaf425a1b39f2035577f2091884016cf1
David Theobald <davidtheo@gmail.com>
2023-11-18 18:28:20 +0000

    updating to display more information

--- routes/web.php
+++ routes/web.php
@@ -21,14 +21,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 
     $posts = Sheets::collection('posts')->all();
-    
+
     return view('posts.index', [
         'posts' => $posts
     ]);
 
 });
 
-Route::get('/posts/{post}', function ($post) {
+Route::get('/posts/{slug}', function ($slug) {
+
+    $post = Sheets::collection('posts')->all()->where('slug', $slug)->first();
+    
+    abort_if(is_null($post), 404);
+
     return view('posts.show', [
         'post' => $post
     ]);
